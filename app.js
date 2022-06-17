6640825ec7c5630bd4421bf688e0f5b527d93c63
Atif Hussain <coderatif@gmail.com>
2022-06-18 02:33:43 +0530

    add it

--- app.js
+++ app.js
@@ -266,88 +266,6 @@ app.post("/send-bulkmsg", async (req, res) => {
       response: "Please Select a valid .vcf/.csv Contacts",
     });
   }
-  //2. userEntered No
-  // if (req.files == null && userEnteredNo[4]) {
-  //   console.log("User Entered Mobile No");
-  //   // format userEntered no in desired form
-  //   let formattedNo = [];
-  //   const UserInputNo = JSON.parse(userEnteredNo);
-  //   UserInputNo.forEach((num) => {
-  //     formattedNo.push(`91${num}@c.us`);
-  //   });
-  //   console.log(formattedNo);
-  //   // send message for each no.
-  //   formattedNo.forEach((singleNo, index, array) => {
-  //     const interval = 5000; // 5 sec wait for each send
-  //     setTimeout(function () {
-  //       console.log(index, singleNo, array.length - 1);
-  //       client
-  //         .sendMessage(singleNo, message)
-  //         .then((response) => {
-  //           if (index == array.length - 1) {
-  //             res.status(200).json({
-  //               status: true,
-  //               response: response,
-  //             });
-  //           }
-  //         })
-  //         .catch((err) => {
-  //           res.status(500).json({
-  //             status: false,
-  //             response: err,
-  //           });
-  //         });
-  //     }, index * interval);
-  //   });
-  // }
-  //3. vcf file No
-  // if (req.files && req.files.file.mimetype == "text/x-vcard") {
-  //   console.log("User Provided vcf contacts");
-  //   vcard.parseVcardFile(req.files.file.tempFilePath, function (err, contacts) {
-  //     if (err) console.log("oops:" + err);
-  //     else {
-  //       // format vcf no in desired form
-  //       let formattedNo = [];
-  //       const results = contacts.filter(
-  //         (contact) => contact.phone[0] != undefined
-  //       );
-  //       results.forEach((result) => {
-  //         formattedNo.push(`${result.phone[0].value}@c.us`);
-  //       });
-  //       const pureIndianFormat = formattedNo.filter((no) => {
-  //         return no.startsWith("+91") && no.length == 20;
-  //       });
-  //       const finalFormattedVcfNo = [];
-  //       pureIndianFormat.forEach((number, index) => {
-  //         let newNo = number.replace("+", "");
-  //         newNo = newNo.replace(/ +/g, "");
-  //         finalFormattedVcfNo.push(newNo);
-  //       });
-  //       finalFormattedVcfNo.forEach((singleNo, index, array) => {
-  //         const interval = 5000; // 5 sec wait for each send
-  //         setTimeout(function () {
-  //           console.log(index, singleNo, array.length);
-  //           client
-  //             .sendMessage(singleNo, message)
-  //             .then((response) => {
-  //               if (index == array.length - 1) {
-  //                 return res.status(200).json({
-  //                   status: true,
-  //                   response: response,
-  //                 });
-  //               }
-  //             })
-  //             .catch((err) => {
-  //               return res.status(500).json({
-  //                 status: false,
-  //                 response: err,
-  //               });
-  //             });
-  //         }, index * interval);
-  //       });
-  //     }
-  //   });
-  // }
   //4. csv file No
   if (req.files && req.files.file.mimetype == "text/csv") {
     console.log("User Provided csv contacts");
@@ -358,7 +276,6 @@ app.post("/send-bulkmsg", async (req, res) => {
     const names = contacts.map((contact) => contact.Name);
     contacts = contacts.map((contact) => `91${contact.Phone}@c.us`);
     contacts.forEach((singleNo, index, array) => {
-      console.log(singleNo, index, array.length);
       const interval = 5000; // 5 sec wait for each send
       // console.log("Hi " + names[index] + " " + message);
       setTimeout(function () {
@@ -368,14 +285,25 @@ app.post("/send-bulkmsg", async (req, res) => {
             "Jai Jinendra " + names[index] + "\n" + message
           )
           .then((response) => {
-            client.sendMessage(singleNo, message2).then((response) => {
-              console.log(response);
-            });
-          })
-          .catch((err) => {
-            console.log(err);
-          });
-      }, index * interval);
+            client
+              .sendMessage(singleNo, message2)
+              .then((response) => {
+                console.log("response ===>", response);
+                if (index == array.length - 1) {
+                  return res.status(200).json({
+                    status: true,
+                    response: response,
+                  });
+                }
+              })
+              .catch((err) => {
+                return res.status(500).json({
+                  status: false,
+                  response: err,
+                });
+              });
+          }, index * interval);
+      });
     });
   }
 });
